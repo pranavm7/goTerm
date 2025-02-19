c78398e0dedebd43234bcda1c460ddec1276605f
pranavm7 <pranavm7@outlook.com>
2025-02-19 14:36:59 -0500

    Added exit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -19,10 +19,19 @@ func ReadUserInput() string {
 	return message
 }
 
+func CheckCommand(command string) {
+	// Check for exit
+	switch command {
+	case "exit 0":
+		os.Exit(0)
+	}
+}
+
 func REPL() {
 	fmt.Fprint(os.Stdout, "$ ")
 	// Using scanner to read the input instead:
 	message := ReadUserInput()
+	CheckCommand(message)
 	message = fmt.Sprintf("%s: command not found", message)
 	_, _ = fmt.Println(message)
 }
