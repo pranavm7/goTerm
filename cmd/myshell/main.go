1828e770d9693a610983f632a66d849b7c759905
pranavm7 <pranavm7@outlook.com>
2025-02-19 13:21:56 -0500

    REPL challenge

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,24 +5,37 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	//"golang.org/x/text/message"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
-func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Using scanner to read the input instead:
+func ReadUserInput() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	message:= scanner.Text()
-	// Wait for user input
-	//reader:= bufio.NewReader(os.Stdin)
-	//message,_:=  reader.ReadString('\n')
-	message= strings.ReplaceAll(message,"\r\n","")
-	message= fmt.Sprintf("%s: command not found",message)
-	//fmt.Fprintf(os.Stdout,  "%s: Command not found",message)
-	_,_ =fmt.Println(message)
+	message := scanner.Text()
+	message = strings.ReplaceAll(message, "\r\n", "")
+	return message
+}
+
+func REPL() {
+	fmt.Fprint(os.Stdout, "$ ")
+	// Using scanner to read the input instead:
+	message := ReadUserInput()
+	message = fmt.Sprintf("%s: command not found", message)
+	_, _ = fmt.Println(message)
+}
+
+func main() {
+	// Uncomment this block to pass the first stage
+	// fmt.Fprint(os.Stdout, "$ ")
+
+	// // Using scanner to read the input instead:
+	// message:= ReadUserInput()
+	// message= fmt.Sprintf("%s: command not found",message)
+	// _,_ =fmt.Println(message)
+	for {
+		REPL()
+	}
 }
